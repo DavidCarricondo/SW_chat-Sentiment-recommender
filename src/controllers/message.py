81eb2d8e7baab1250bdd5e99f102376328934d98
DavidCarricondo <dacsmail00@gmail.com>
2020-05-01 20:01:40 +0200

    Added some error handling

--- src/controllers/message.py
+++ src/controllers/message.py
@@ -18,23 +18,34 @@ def add_message(user):
     chat = request.args['chat']
     text = request.args['text']
 
+    #Check that the user exist
+    cur = db.users.find({}, {'name':1})
+    if user not in [n['name'] for n in cur]:
+        raise APIError ('That user does not exist, you can create it with the endpoint /user/create/<user_name>')
+
     id_chat = db.chats.find_one({'name':chat},{'_id':1})['_id']
     id_user = db.users.find_one({'name':user},{'_id':1})['_id']
 
+    #Check that the user exist in the chat and otherwise, include it:
+    participants = db.chats.find_one({'$and':[{'_id':id_chat},{'participants':id_user}]})
+    if participants == None:
+            db.chats.update({'_id': id_chat },{'$addToSet': {'participants': id_user } })
+            message = 'We succeded in adding the message and added the user to the chat'
+    else: message = 'We succeded in adding the message'
+    
     db.messages.insert_one({'chat': id_chat,'user': id_user,'time': str(datetime.datetime.now()),'text': text})
+    return {'status': message,
+            'entry': dumps(db.messages.find_one({'text':text}))}
+ 
 
-    return {'message':'We succeeded in adding the text',
-            'entry': dumps(db.messages.find_one({'text':text}))
-            }
 
-
-@app.route('/message/list/<chat>')
+@app.route('/message/<chat>/list/')
 @errorHandler
 def list_messages(chat):
 
     id_chat = db.chats.find_one({'name':chat},{'_id':1})['_id']
     messages = db.messages.find({'chat':id_chat})
-    
+
     return dumps(messages)
 
 
